5ae5d9d3519575ef60c530fed23f185a623989ac
Alexandre Mouriec <alexandre.mouriec@gmail.com>
2024-05-25 18:41:31 +0200

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import vue from "@astrojs/vue";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://alexandremouriec.com",
-  integrations: [tailwind(), vue()]
+  integrations: [tailwind(), vue(), sitemap()]
 });
