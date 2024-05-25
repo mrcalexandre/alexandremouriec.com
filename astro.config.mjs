46918b68a835820769d058d3fa46413f31809b9b
Alexandre Mouriec <alexandre.mouriec@gmail.com>
2024-05-25 19:11:36 +0200

    feat: allow full content in my rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://alexandremouriec.com",
-  integrations: [tailwind(), vue(), sitemap()]
+  integrations: [tailwind(), vue(), sitemap()],
+  trailingSlash: "always"
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,23 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
-  const posts = await getCollection("posts");
+  const blog = await getCollection('posts');
   return rss({
-    title: 'Astro Learner | Blog',
-    description: 'My journey learning Astro',
+    title: 'Alexandre Mouriec',
+    description: 'A blog',
     site: context.site,
-    items: posts.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
+    stylesheet: '/rss/pretty-feed-v3.xsl',
+    items: blog.map((post) => ({
       link: `/blog/${post.slug}/`,
+      // Note: this will not process components or JSX expressions in MDX files.
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      }),
+      ...post.data,
     })),
-    customData: `<language>en-us</language>`,
-  })
+  });
 }
