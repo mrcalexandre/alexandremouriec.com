c252df91eb6c523bb1039c6a2d433ccd91b4cb7a
Alexandre Mouriec <alexandre.mouriec@gmail.com>
2024-05-25 22:42:15 +0200

    feat: Add last modified time via remark plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
+import { remarkModifiedTime } from './src/scripts/remark-modified-time.mjs';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://alexandremouriec.com",
   integrations: [tailwind(), vue(), sitemap()],
-  trailingSlash: "always"
+  markdown: {
+    remarkPlugins: [remarkModifiedTime],
+  }
 });
