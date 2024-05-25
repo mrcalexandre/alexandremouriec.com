da288d22381d572d7e36ccbcf47956cb57ac9854
Alexandre Mouriec <alexandre.mouriec@gmail.com>
2024-05-25 23:45:15 +0200

    fix: remove remark plugin for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,10 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import { defineConfig } from 'astro/config';
-import { remarkModifiedTime } from './src/scripts/remark-modified-time.mjs';
 
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://alexandremouriec.com",
   integrations: [tailwind(), vue(), sitemap()],
-  markdown: {
-    remarkPlugins: [remarkModifiedTime],
-  }
 });
