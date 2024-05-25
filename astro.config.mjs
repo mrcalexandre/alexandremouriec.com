c294c1dfdc6f546ab14ef5f22601e4c2932a34ff
Alexandre Mouriec <alexandre.mouriec@gmail.com>
2024-05-25 16:26:30 +0200

    feat: add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://alexandremouriec.com"
-
+  site: "https://alexandremouriec.com",
+  integrations: [tailwind()]
 });
