eea6a2fab77cdfed5e1388651289cb4e5ff50222
Alexandre Mouriec <alexandre.mouriec@gmail.com>
2024-05-25 18:02:17 +0200

    feat: add vue support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://alexandremouriec.com",
-  integrations: [tailwind()]
+  integrations: [tailwind(), vue()]
 });
