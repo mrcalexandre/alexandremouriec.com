c4ae612cb374afbc6a5200e041aa35755fa990fa
Alexandre Mouriec <alexandre.mouriec@gmail.com>
2024-05-26 00:17:48 +0200

    feat: add analytics via vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,16 @@ import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://alexandremouriec.com",
   integrations: [tailwind(), vue(), sitemap()],
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
