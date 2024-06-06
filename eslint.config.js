dda378f3a93ab57bb4838cc3aa0ffb4d19414afd
Alexandre Mouriec <alexandre.mouriec@gmail.com>
2024-06-06 15:18:35 +0200

    chore: update eslint and prettier configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,16 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+import perfectionist from "eslint-plugin-perfectionist";
+import perfectionistNatural from "eslint-plugin-perfectionist/configs/recommended-natural";
 export default [
   // add more generic rule sets here, such as:
   // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+  perfectionistNatural,
   {
-    rules: {
-      // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
+    plugins: {
+      perfectionist,
     },
-  },
+    rules: {
+    },
+  }
 ];
