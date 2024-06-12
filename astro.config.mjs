753249a51f03cb5c0bf97707e9e80aec7cfbb76b
Alexandre Mouriec <alexandre.mouriec@gmail.com>
2024-06-12 10:24:14 +0200

    feat(analytics): disable vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,15 @@
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
-import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import { defineConfig } from "astro/config";
 
 import { remarkModifiedTime } from "./src/utils/remark-modified-time.mjs";
 import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    }
-  }),
+  adapter: vercel(),
   integrations: [tailwind(), sitemap(), icon({
     iconDir: "src/images/icons"
   })],
