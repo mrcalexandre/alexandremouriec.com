ae2631098dfc08a8c2345ba77eb4b04ae6aedfc7
Alexandre Mouriec <alexandre.mouriec@gmail.com>
2024-06-26 15:50:25 +0200

    chore: remove vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
@@ -9,7 +8,6 @@ import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
   integrations: [tailwind(), sitemap(), icon({
     iconDir: "src/images/icons"
   })],
