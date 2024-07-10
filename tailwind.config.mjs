15c777f17ad77a1ea6d1630a6c2e30ebf2b0b8df
Alexandre Mouriec <alexandre.mouriec@gmail.com>
2024-07-10 14:56:39 +0200

    chore: update tailwind colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,12 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: ["class"],
+  extend: {
+    colors: {
+      "dark": "#2E2E2E",
+      "light": "#fff7f3",
+    },
+  },
   plugins: [
     require("@tailwindcss/typography"),
   ],
