a7c2b080ccd4e661d23e0d27aa7cde3359e029ab
Alexandre Mouriec <alexandre.mouriec@gmail.com>
2024-07-18 16:52:38 +0200

    feat: improve layout

--- src/content/posts/new-website.md
+++ src/content/posts/new-website.md
@@ -3,7 +3,7 @@ title: I have built a new website
 author: Alexandre Mouriec
 pubDate: 2024-07-10
 description: This blogpost explains how I built this website and what I plan to do with it.
-tags: ["programming"]
+tags: ["website", "indieweb"]
 ---
 
 Welcome to my _new personal website_ !
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,20 @@
-
 /** @type {import("tailwindcss").Config} */
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: ["class"],
-  extend: {
-    colors: {
-      "dark": "#2e2e2e",
-      "light": "#fff7f3",
-    },
-  },
   plugins: [
     require("@tailwindcss/typography"),
   ],
   safelist: ["dark"],
+  theme: {
+    extend: {
+      fontSize: {
+        "xs": "12px"
+      },
+      screens: {
+        "xxs": "525px"
+      }
+    }
+  },
 };
