6f93c36766a4f38575ebd6bacbe096a856a30d61
Alexandre Mouriec <19906367+mrcalexandre@users.noreply.github.com>
2024-10-21 22:44:15 +0200

    feat(config): use resume url to redirect to resume file

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,6 +15,9 @@ export default defineConfig({
     remarkPlugins: [remarkModifiedTime, remarkReadingTime]
   },
   output: "static",
+  redirects: {
+    "/resume": "https://kdrive.infomaniak.com/app/share/1247749/e07325e1-160f-42ae-a0c8-7722811139af"
+  },
   site: "https://alexandremouriec.com",
   trailingSlash: "never"
 });
